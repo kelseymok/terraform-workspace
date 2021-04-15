5c3e00153a946ea8caee97f6f04eb14b062b5204
kmok <kmok@thoughtworks.com>
2021-04-15 15:39:40 +0200

    km: removing extra vars in role session name

--- assume-role.sh
+++ assume-role.sh
@@ -12,7 +12,7 @@ account=$(aws sts get-caller-identity | jq -r '.Account')
 
 response=$(aws sts assume-role \
 --role-arn "arn:aws:iam::${account}:role/${role}" \
---role-session-name "terraform_runner-${CI_PIPELINE_ID}-${CI_JOB_ID}")
+--role-session-name "github-runner")
 
 aws configure set aws_access_key_id $(echo $response | jq -r '.Credentials.AccessKeyId') --profile default
 aws configure set aws_secret_access_key $(echo $response | jq -r '.Credentials.SecretAccessKey') --profile default
