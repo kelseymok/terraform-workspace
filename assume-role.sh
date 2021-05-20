419d11faf7c1642738219e5b4e7d3eb0bd89ccef
Kelsey Mok <kelseymok@gmail.com>
2021-05-20 15:41:11 +0200

    km: adding saml2aws

--- assume-role.sh
+++ assume-role.sh
@@ -12,7 +12,7 @@ account=$(aws sts get-caller-identity | jq -r '.Account')
 
 response=$(aws sts assume-role \
 --role-arn "arn:aws:iam::${account}:role/${role}" \
---role-session-name "github-runner")
+--role-session-name "local-terraform-workspace")
 
 aws configure set aws_access_key_id $(echo $response | jq -r '.Credentials.AccessKeyId') --profile default
 aws configure set aws_secret_access_key $(echo $response | jq -r '.Credentials.SecretAccessKey') --profile default
